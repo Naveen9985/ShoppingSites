99666483b6beec2a420354eb528bf18686e398d7
admin <admin@Nav>
2019-12-28 00:47:58 +0530

    Adding Iphone to amazon and flipkart and comparing the price

---
+++ Assignment1/src/com/pages/Amazon.java
@@ -0,0 +1,40 @@
+package com.pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+ 
+import com.utility.BaseClass;
+
+public class Amazon extends BaseClass{
+   public static int Aprice;
+public static Integer amazonPrice ;
+
+public static void openAmazon() throws Exception{
+	driver.get("https://www.amazon.in/");
+	//Waiting for the page to be loaded completely
+	System.out.println("Opened Amazon");
+	 driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+	 driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+	 
+	 driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("iPhone XR (64GB) - Yellow");
+	 //Mouse Hover To click on search 
+	 Actions action = new Actions(driver);
+	 WebElement ele = driver.findElement(By.xpath("//input[@type = 'submit']"));
+	 action.moveToElement(ele).click().build().perform();
+	 System.out.println("Clicked on search");
+	 
+	 String ele1 = driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[4]/div[1]/div[1]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]")).getText();
+	 Thread.sleep(5000);
+	 
+	 System.out.println("The price of the selected product in Amazon is : " + ele1);
+	 
+	ele1 =ele1.replaceAll("[^a-zA-Z0-9]", "");
+	System.out.println(ele1);
+	 amazonPrice = new Integer(ele1);
+	 Aprice= amazonPrice.intValue();
+	System.out.println(Aprice);
+}
+}
---
+++ Assignment1/src/com/utility/BaseClass.java
@@ -0,0 +1,29 @@
+package com.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.pages.Amazon;
+import com.pages.Flipkart;
+
+public class BaseClass {
+   public static WebDriver driver;
+   
+	public static void openBrowser() {
+	System.setProperty("webdriver.chrome.driver", "E:\\Selenium_Student_SW\\Drivers\\chromedriver.exe");
+	driver = new ChromeDriver();
+	System.out.println("lauched browser");
+}
+	public static void closeApp() {
+		driver.close();
+		System.out.println("Application closed");
+	}
+	public static void compare() {
+		if(Amazon.Aprice < Flipkart.Fprice) {
+			System.out.println("The price of the product is less in Amazon");
+		}else
+		{
+			System.out.println("The price of the product is less in Flipkart");
+		}
+	}
+}
