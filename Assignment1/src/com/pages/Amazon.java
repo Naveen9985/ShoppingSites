bfbe8027c97aa0d5ea8f871f38527204294b8e5f
admin <admin@192.168.2.28>
2019-12-30 10:51:13 +0530

    Added some logs

--- Assignment1/src/com/pages/Amazon.java
+++ Assignment1/src/com/pages/Amazon.java
@@ -5,7 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
- 
+import org.testng.Reporter;
+
 import com.utility.BaseClass;
 
 public class Amazon extends BaseClass{
@@ -13,9 +14,12 @@ public class Amazon extends BaseClass{
 public static Integer amazonPrice ;
 
 public static void openAmazon() throws Exception{
+	
 	driver.get("https://www.amazon.in/");
 	//Waiting for the page to be loaded completely
-	System.out.println("Opened Amazon");
+	 System.out.println("Opened Amazon");
+	 Reporter.log("Opened Amazon");
+	 
 	 driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 	 driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 	 
@@ -25,16 +29,17 @@ public static void openAmazon() throws Exception{
 	 WebElement ele = driver.findElement(By.xpath("//input[@type = 'submit']"));
 	 action.moveToElement(ele).click().build().perform();
 	 System.out.println("Clicked on search");
-	 
+	 Reporter.log("Clicked on search");
 	 String ele1 = driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[4]/div[1]/div[1]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]")).getText();
 	 Thread.sleep(5000);
 	 
 	 System.out.println("The price of the selected product in Amazon is : " + ele1);
-	 
+	 Reporter.log("The price of the selected product in Amazon is : " + ele1);
 	ele1 =ele1.replaceAll("[^a-zA-Z0-9]", "");
 	System.out.println(ele1);
 	 amazonPrice = new Integer(ele1);
 	 Aprice= amazonPrice.intValue();
 	System.out.println(Aprice);
+    Reporter.log("Iphone Price in Amazon is :" + Aprice);
 }
 }
--- Assignment1/src/com/utility/BaseClass.java
+++ Assignment1/src/com/utility/BaseClass.java
@@ -2,6 +2,7 @@ package com.utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 
 import com.pages.Amazon;
 import com.pages.Flipkart;
@@ -17,13 +18,16 @@ public class BaseClass {
 	public static void closeApp() {
 		driver.close();
 		System.out.println("Application closed");
+		Reporter.log("Application closed");
 	}
 	public static void compare() {
 		if(Amazon.Aprice < Flipkart.Fprice) {
 			System.out.println("The price of the product is less in Amazon");
+			Reporter.log("The price of the product is less in Amazon");
 		}else
 		{
 			System.out.println("The price of the product is less in Flipkart");
+			Reporter.log("The price of the product is less in Flipkart");
 		}
 	}
 }
